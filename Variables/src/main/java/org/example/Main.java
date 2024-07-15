df1b28087033594a4bb3a45427bbae4ba70d446f
navanith12 <navanithrao337@gmail.com>
2024-07-14 23:24:02 -0500

    variables

---
+++ Variables/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+//package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main (String[] args){
+        String name = "Navanith";
+        int number = 4;
+        float digit = 3.85f;
+        System.out.println(name);
+        System.out.println(number);
+        System.out.println(digit);
+    }
+
+}
